1caee7db9bf8d28eca396ded93168766338eecb1
hupark <phwslk@gmail.com>
2022-12-27 20:34:58 +0900

    notion-api 연동

---
+++ src/utils/notion-api.js
@@ -0,0 +1,13 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+});
+
+export const getBlocks = async (blockId) => {
+    const response = await notion.blocks.children.list({
+        block_id: blockId,
+        page_size: 50,
+    });
+    return response.results;
+};
