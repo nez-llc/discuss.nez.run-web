c64436f02f2e1c112bd82569789ee77ef833241f
hupark <phwslk@gmail.com>
2023-03-01 12:42:49 +0900

    dayjs 분리 배너1,2 리스트1,2,3 수정 배너 swiper 라이브러리 추가

--- src/components/question/QuestionPreview.js
+++ src/components/question/QuestionPreview.js
@@ -3,10 +3,16 @@ import styled from '@emotion/styled'
 import {css} from '@emotion/react'
 import Tags from 'components/ui/Tags'
 import VoteBar from 'components/ui/VoteBar'
-import dayjs from 'dayjs'
-import duration from 'dayjs/plugin/duration'
+import {detailDate} from 'utils/dayjs'
 
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 0px 10px;
+  /*border-bottom: 1px solid #bdbdbd;*/
+`
 const Recommended = styled.h2`
 `
 
@@ -20,113 +26,124 @@ const Info = styled.div`
 const sum = arr => arr.reduce((a, b) => a + b, 0)
 
 
-const detailDate = (agendaDate) => {
-  dayjs.extend(duration)
-
-  let timeDiff = dayjs.duration(dayjs().diff(agendaDate))
-  let result = ''
-
-  if(timeDiff.years() > 0){
-    result = `${timeDiff.years()}년 전`
-  }else if(timeDiff.months() > 0){
-    result = `${timeDiff.months()}개월 전`
-  }else if(timeDiff.days() > 0){
-    result = `${timeDiff.days()}일 전`
-  }else if(timeDiff.hours() > 0){
-    result = `${timeDiff.hours()}시간 전`
-  }else if(timeDiff.minutes() > 0){
-    result = `${timeDiff.minutes()}분 전`
-  }else if(timeDiff.seconds() > 0){
-    result = `${timeDiff.seconds()}초 전`
-  }
-
-  return result
-}
-
 const Stat = ({ question }) => {
   return (
-    <div
-      css={css`
-      font-size: small;
-      dl {
-        display: flex;
-      }
-      dd {
-        margin-right: 16px;
-      }
-    `}
-    >
+    <div css={css`font-size: small; dl {display: flex;} dd {margin-right: 16px;}`}>
       <dl>
         <dt>추천</dt>
         <dd>{question.updown?.total}</dd>
-        <dt>투표</dt>
-        <dd>{sum(Object.values(question.vote_count))}</dd>
+        {/* <dt>투표</dt>
+        <dd>{sum(Object.values(question.vote_count))}</dd> */}
         <dt>댓글</dt>
         <dd>{question.comment_count}</dd>
-        <dt css={css`margin-left: auto;`}>{detailDate(question.created_time)}</dt>
       </dl>
     </div>
   )
 }
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-  gap: 15px;
-`
-
-const QuestionPreview = ({ question }) => (
-  <Wrapper>
-    <div css={css`
-      display: flex;
-      gap: 5px;
-    `}>
-      <div css={css`
-            width:30%;
-          `}>
-        <img css={css`
-          height: 100px;
-          object-fit: cover;
+const ListView = ({ question }) => (
+  <>
+    <div css={css`display: flex;`}>
+      <div css={css`width: 160px; height: 160px;`}>
+        <img css={css`width: 100%; height: 100%; object-fit: cover;
         `} src={'https://cdn.pixabay.com/photo/2019/07/23/13/51/shepherd-dog-4357790_1280.jpg'}/>
       </div>
-      <div css={css`
-            width:70%;
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-          `}>
-        <h4>{question.title}</h4>
-        <p css={css`
-          overflow: hidden;
-          text-overflow: ellipsis;
-          display: -webkit-box;
-          -webkit-line-clamp: 2; 
-          -webkit-box-orient: vertical;
-        `}>{question.desc}</p>
+      <div css={css`width:70%; display: flex; flex-direction: column; gap: 10px;`}>
+        <div css={css`width: 100%; padding: 0px 10px; height: 134px;`}>
+          <h4 css={css`height: 80px; display: flex; align-items: self-end;`}>{question.title}</h4>
+          <div css={css`padding-top: 10px;`}>
+            {/* <Tags tags={question.tags}></Tags> */}
+            <ul css={css`display: flex; gap: 5px;`}>
+              {question.tags?.slice(0, 3).map(tag => (
+                <li key={tag.id} css={css`color: #FF6861; border: 1px solid #FF6861; padding: 3px 5px; border-radius: 20px; font-size: x-small;`}>{tag.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <VoteBar voteCount={question.vote_count} />
       </div>
     </div>
-    <div css={css`
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      gap: 10px;
-    `}>
+  </>
+)
+
+const ListView2 = ({ question }) => (
+  <>
+    <div css={css`display: flex; gap: 5px;`}>
+      <div css={css`width:70%; display: flex; flex-direction: column;`}>
+        <h4 css={css`height: 70px`}>{question.title}</h4>
+        {/* <Tags tags={question.tags}></Tags> */}
+        <ul css={css`display: flex; gap: 5px;`}>
+          {question.tags?.slice(0, 3).map(tag => (
+            <li key={tag.id} css={css`color: #FF6861; border: 1px solid #FF6861; padding: 3px 5px; border-radius: 20px; font-size: x-small;`}>{tag.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div css={css`width:30%;`}>
+        <img css={css`height: 100px; object-fit: cover;`}
+          src={'https://cdn.pixabay.com/photo/2019/07/23/13/51/shepherd-dog-4357790_1280.jpg'}/>
+      </div>
+    </div>
+    <div css={css`width: 100%; display: flex; flex-direction: column; gap: 10px;`}>
+      {/* <Stat question={question} /> */}
+      <div css={css`font-size: small; dl {display: flex;} dd {margin-right: 16px;}`}>
+        <dl>
+          <dt>추천</dt>
+          <dd>{question.updown?.total}</dd>
+          <dt>투표</dt>
+          <dd>{sum(Object.values(question.vote_count))}</dd>
+          <dt>댓글</dt>
+          <dd>{question.comment_count}</dd>
+          <dt css={css`margin-left: auto;`}>{detailDate(question.created_time)}</dt>
+        </dl>
+      </div>
       <VoteBar voteCount={question.vote_count} />
-      <Tags tags={question.tags}></Tags>
+    </div>
+  </>
+)
+
+const ListView3 = ({ question }) => (
+  <>
+    <div css={css`display: flex; gap: 5px;`}>
+      <div css={css`width:70%; display: flex; flex-direction: column; gap: 10px;`}>
+        <h4 css={css`height: 70px`}>{question.title}</h4>
+        <ul css={css`display: flex; gap: 3px;`}>
+          <li css={css`width:20%; font-size: 11px; text-align: center;`}>{detailDate(question.created_time)}</li>
+          <li css={css`width: 60%;`}>
+            <VoteBar voteCount={question.vote_count} />
+          </li>
+          <li css={css`width:20%; font-size: small; text-align: center;`}>{sum(Object.values(question.vote_count))}</li>
+        </ul>
+      </div>
+      <div css={css`width:30%;`}>
+        <img css={css`height: 100px; object-fit: cover;`}
+          src={'https://cdn.pixabay.com/photo/2019/07/23/13/51/shepherd-dog-4357790_1280.jpg'}/>
+      </div>
+    </div>
+    <div css={css`width: 100%; display: flex; flex-direction: column; gap: 10px;`}>
+      {/* <Tags tags={question.tags}></Tags> */}
+      <ul css={css`display: flex; gap: 5px;`}>
+        {question.tags?.slice(0, 3).map(tag => (
+          <li key={tag.id} css={css`font-size: small; text-decoration: underline; color: #828282;`}>{tag.name}</li>
+        ))}
+      </ul>
       <Stat question={question} />
     </div>
-    {/* <div> */}
-    {/*  <Recommended>{question.recommended}</Recommended> */}
-    {/* </div> */}
-
-    {/* <Info> */}
-    {/*   <h3><img src={''} />{question.title}</h3> */}
-    {/*   <Markdown>{question.desc}</Markdown> */}
-    {/* </Info> */}
-    {/* <Tags tags={question.tags} /> */}
-    {/* <VoteBar voteCount={question.vote_count} /> */}
-    {/* <Stat question={question} /> */}
+  </>
+)
+const QuestionPreview = ({ question, view }) => (
+  <Wrapper>
+    {view === 'L01' ?
+      <ListView question={question} />
+      : view === 'L02' ?
+        <>
+          <ListView2 question={question} />
+          <hr css={css`width:100%; border: 0; border-top: 1px solid #C4C4C4; margin: 0;`} />
+        </>
+        : <>
+          <ListView3 question={question} />
+          <hr css={css`width:100%; border: 0; border-top: 1px solid #C4C4C4; margin: 0;`} />
+        </>
+    }
   </Wrapper>
 )
 
--- src/components/question/QustionList.js
+++ src/components/question/QustionList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
@@ -7,9 +7,17 @@ import QuestionPreview from 'components/question/QuestionPreview'
 import { useQuestions } from 'data/questions'
 import Router, { useRouter } from 'next/router'
 import MainQuestionPreview from './MainQuestionPreview'
+import Tags from '../ui/Tags'
+import VoteBar from '../ui/VoteBar'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper.css'
+import SwiperCore, { Navigation } from 'swiper'
+import { detailDate } from 'utils/dayjs'
 
 const Wrapper = styled.div`
   padding: 16px 0;
+  font-family: 'Source Sans Pro';
+  font-style: normal;
 `
 const MainUl = styled.ul`
   display: grid;
@@ -29,6 +37,14 @@ const MainWrapper = styled.div`
 `
 const Text = styled.p`
   margin: 0 0 5px 65px;
+`
+
+const SetViewButtons = styled.div`
+  display: flex;
+  gap: 5px;
+  button{
+    flex-direction: column;
+  }
 `
 
 const OrderSelect = ({sort}) => {
@@ -58,12 +74,12 @@ const OrderSelect = ({sort}) => {
   )
 }
 
-const List = ({ questions }) => (
+const List = ({ questions, view }) => (
   <ul
     css={css`
         display: flex;
         flex-direction: column;
-        gap: 15px;
+        gap: 10px;
         a {
           text-decoration: none;
         }
@@ -72,16 +88,126 @@ const List = ({ questions }) => (
     {questions.map(question => (
       <li key={question.id}>
         <Link href={`/agenda/${question.id}`}>
-          <QuestionPreview key={question.id} question={question} />
+          <QuestionPreview key={question.id} question={question} view={view} />
         </Link>
       </li>
     ))}
   </ul>
 )
 
+const SwiperList = ({ questions }) => {
+  SwiperCore.use([Navigation])
+  return (
+    <Swiper>
+      {questions.map((question) => (
+        <SwiperSlide key={question.id}> {/* key 중복 error 발생으로 임시로 key 추가 */}
+          <Link key={question.id} href={`/agenda/${question.id}`} css={css`text-decoration: none;`}>
+            <div css={css`background-color: #bdbdbd; background-image: url("https://cdn.pixabay.com/photo/2019/07/23/13/51/shepherd-dog-4357790_1280.jpg"); background-repeat: none; background-size: cover;`}>
+              <div
+                css={css`
+                  height: 250px;
+                  background-position: 50% 50%;
+                  background-size: contain;
+                  background-repeat: no-repeat;
+                  flex: none;
+                  padding: 20px 5px;
+                  display: grid;
+                  gap: 15px;
+                `}>
+                <div css={css`padding: 0px 20px; display:flex`}>
+                  <p css={css`margin-left: auto; color: #fff`}>투표하러 가기</p>
+                </div>
+                <h2 css={css`color: #fff; min-height: 80px; display: flex; align-items: self-end;`}>{question.title}</h2>
+                <VoteBar voteCount={question.vote_count} />
+                {/* <Tags tags={question.tags} /> */}
+                <ul css={css`display: flex; gap: 5px; height: 25px;`}>
+                  {question.tags?.slice(0, 3).map(tag => (
+                    <li key={tag.id} css={css`font-size: small; color: #fff; border: 1px solid #fff; padding: 3px 5px; border-radius: 20px;`}>{tag.name}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div css={css`font-size: small; dl {display: flex;} dd {margin-right: 16px;} line-height: 50px; padding: 0px 10px;`}>
+              <dl>
+                <dt>추천</dt>
+                <dd>{question.updown?.total}</dd>
+                <dt>투표</dt>
+                <dd>0</dd>
+                <dt>댓글</dt>
+                <dd>{question.comment_count}</dd>
+                <dt css={css`margin-left: auto;`}>{detailDate(question.created_time)}</dt>
+              </dl>
+            </div>
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+}
+
+const SwiperList2 = ({ questions }) => {
+  SwiperCore.use([Navigation])
+  return (
+    <Swiper>
+      {questions.map((question) => {
+        return (
+          <SwiperSlide key={question.id}> {/* key 중복 error 발생으로 임시로 key 추가 */}
+            <Link key={question.id} href={`/agenda/${question.id}`} css={css`text-decoration: none;`}>
+              <div css={css`background-color: #bdbdbd; background-image: url("https://cdn.pixabay.com/photo/2019/07/23/13/51/shepherd-dog-4357790_1280.jpg"); background-repeat: none; background-size: cover;`}>
+                <div
+                  css={css`
+                  height: 300px;
+                  background-position: 50% 50%;
+                  background-size: contain;
+                  background-repeat: no-repeat;
+                  flex: none;
+                  padding: 20px 5px;
+                  display: grid;
+                  gap: 15px;
+              `}>
+                  <div css={css`padding: 0px 20px; display:flex`}>
+                    <ul css={css`display: flex; gap: 5px; height: 25px;`}>
+                      {question.tags?.slice(0, 3).map(tag => (
+                        <li key={tag.id} css={css`font-size: small; color: #fff; border: 1px solid #fff; padding: 3px 5px; border-radius: 20px;`}>{tag.name}</li>
+                      ))}
+                    </ul>
+                    <dl css={css`margin-left: auto; color: #fff; display: flex; font-weight: 700;`}>
+                      <dt>추천</dt>
+                      <dd>{question.updown?.total}</dd>
+                    </dl>
+                  </div>
+                  <h2 css={css`color: #fff; padding: 0px 20px; min-height: 120px; display: flex; align-items: self-end;`}>{question.title}</h2>
+                  <div css={css`display: flex;
+                  align-items: center;
+                  padding: 8px 12px;
+                  gap: 8px;
+                  width: 203px;
+                  height: 37px;
+                  background: linear-gradient(315deg, #FF6861 -0.12%, #FF8B86 99.88%);
+                  border-radius: 32px;
+                  margin: auto;
+                  font-size: 14px;
+                  p {flex-direction: column;}
+                `}>
+                    <p css={css`width: 100%;
+                    text-align: center;
+                    color: #fff;
+                    font-weight: 600;
+                    font-size: 14px;
+                  `}>반대 85% &gt; 투표 하러가기</p>
+                  </div>
+                  {/* <VoteBar voteCount={question.vote_count} /> */}
+                </div>
+              </div>
+            </Link>
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
+  )
+}
 
 const MainList = ({ questions }) =>
-  // questions = questions.slice(0, 6)
   (
     <MainUl>
       {questions.slice(0, 6).map((question, index) => (
@@ -96,6 +222,11 @@ const MainList = ({ questions }) =>
 
 const QuestionList = ({tag, keyword, sort, searchType, view}) => {
   const { questions, total, per_page } = useQuestions(tag, keyword, sort, searchType)
+  const [ viewState, setViewState ] = useState('S01')
+
+  const changeView = (state) => {
+    setViewState(state)
+  }
 
   return (
     <Wrapper>
@@ -109,9 +240,28 @@ const QuestionList = ({tag, keyword, sort, searchType, view}) => {
           <>
             <a href="/agenda/new">새로운 질문 올리기</a>
             <hr />
+            <SetViewButtons>
+              <button onClick={() => changeView('S01')}>배너1</button>
+              <button onClick={() => changeView('S02')}>배너2</button>
+              <button onClick={() => changeView('L01')}>리스트1</button>
+              <button onClick={() => changeView('L02')}>리스트2</button>
+              <button onClick={() => changeView('L03')}>리스트3</button>
+            </SetViewButtons>
             <OrderSelect sort={sort} />
-            <List questions={questions}/>
-            <Pagination total={total} per_page={per_page}/>
+            {viewState === 'S01'
+              ? <>
+                {/* <SlideList questions={questions} /> */}
+                <SwiperList questions={questions} />
+              </>
+              : viewState === 'S02'
+                ? <>
+                  <SwiperList2 questions={questions} />
+                </>
+                : <>
+                  <List questions={questions} view={viewState}/>
+                  <Pagination total={total} per_page={per_page}/>
+                </>
+            }
           </>
       }
     </Wrapper>
