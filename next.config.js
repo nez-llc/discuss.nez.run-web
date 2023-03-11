768126c6d705cfb1a2bc27f526a743cf1f56ed8c
sl kim <sl@lxf.kr>
2023-03-11 15:49:14 +0900

    Notion API키 next에서 접근 가능하게 수정

--- next.config.js
+++ next.config.js
@@ -8,5 +8,6 @@ module.exports = {
     FB_CLIENT_SECRET: process.env.FB_CLIENT_SECRET,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    NOTION_API_KEY: process.env.NOTION_API_KEY,
   },
 }
