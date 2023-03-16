5b5d717678bc7d8af2104549063edea721dad8c3
hupark <phwslk@gmail.com>
2023-03-16 23:48:19 +0900

    tags view props 추가, ui 변경

--- src/components/ui/Tags.js
+++ src/components/ui/Tags.js
@@ -4,32 +4,37 @@ import Link from 'next/link'
 import {useRouter} from 'next/router'
 
 const Ul = styled.ul`
-  padding-top: 20px;
   display: flex;
   gap: 10px;
   margin-bottom: 15px;
-  //justify-content: flex-end;
-  //margin-bottom: 15px;
   a{
     text-decoration: none;
   }
 `
 
 const Li = styled.li`
-  font-size: small;
-  padding: 4px 12px;
-  height: 29px;
-  border: 2px solid #2F80ED;
-  color: #2F80ED;
-  border-radius: 24px;
+  ${props => props.view === 'list' ? `
+      text-decoration-line: underline;
+      color: #828282;
+      font-size: 12px;
+    `
+    : `
+      padding: 4px 12px;
+      height: 29px;
+      border: 2px solid #2F80ED;
+      color: #2F80ED;
+      border-radius: 24px;
+      font-size: 14px;
+  `};
 `
 
-const Tags = ({ tags }) => {
+const Tags = ({ tags, view }) => {
   const router = useRouter()
+
   return (
     <Ul>
-      {tags && tags.map((tag, index) => (
-        <Li key={tag.id}>
+      {tags && tags.map((tag) => (
+        <Li key={tag.id} view={view}>
           <Link href={{
             pathname: router.pathname,
             query: { ...router.query, tag: tag.name },
