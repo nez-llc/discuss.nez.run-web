2802dbc6de9a008419286b163bdfd51560c0ef98
hupark <phwslk@gmail.com>
2023-03-17 22:19:53 +0900

    logo 및 header 디자인 변경

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import styled from '@emotion/styled'
-
+import LogoImg from 'assets/logo_dark.png'
+import MainLogoImg from 'assets/logo_white.png'
+import Image from 'next/image'
 
 const Div = styled.div`
   font-size: 26px;
+  width: 60px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  
+  img{
+    height: fit-content;
+  }
 `
 
-const Logo = () => (
-  <Div>
-    WooDongD
-  </Div>
-)
+const Logo = (props) => {
+  const isMain = props.isMain
+  return (
+    <Div>
+      <Image
+        src={isMain ? MainLogoImg : LogoImg} alt={'우동디 로고'}/>
+    </Div>
+  )
+}
 
 export default Logo
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,15 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Link from 'next/link'
 import styled from '@emotion/styled'
 import Logo from 'components/Logo'
 import Account from 'components/member/Account'
 import SearchImg from 'assets/search.png'
 import MenuImg from 'assets/menu.png'
-import FilterImg from 'assets/filter_list.png'
-import AccountImg from 'assets/person.png'
+import MainSearchImg from 'assets/search_white.png'
+import MainMenuImg from 'assets/menu_white.png'
 import NavMenu from 'components/ui/NavMenu'
+import Router, {useRouter} from 'next/router'
+import Image from 'next/image'
 
 const Wrapper = styled.header`
   background: white;
@@ -17,15 +19,15 @@ const Wrapper = styled.header`
   align-items: center;
   //padding: 8px;
   //box-shadow: ${({ theme }) => theme.shadow};
-  padding: 0 100px;
+  padding: 0 40px;
   border-bottom: 1px solid black;
   height: 75px;
-  @media screen and (max-width: 900px) {
-    padding: 15px;
-    height: 100%;
-    flex-direction: column;
-    border: 0;
-  }
+    @media screen and (max-width: 900px) {
+      padding: 15px;
+      height: 100%;
+      flex-direction: column;
+      border: 0;
+    }
 `
 
 const Nav = styled.nav`
@@ -33,7 +35,6 @@ const Nav = styled.nav`
   gap: 16px;
   flex-grow: 1;
   position: relative;
-  justify-content: ${props => props.headerView === 3 ? 'center' : 'normal'};
   a {
     text-decoration: none;
     cursor: pointer;
@@ -54,16 +55,15 @@ const Tools = styled.div`
 const NavBox = styled.div`
   align-items: center;
   display: flex;
-  right: 0;
-  position: absolute;
-  margin-top: 7px;
+  gap: 10px;
+  margin-left: auto;
+  ${props => props.isMain && `
+    color: #fff;
+  `};
+  
   @media screen and (max-width: 900px) {
     display: none;
   }
-`
-
-const A = styled.a`
-  margin: 0 15px;
 `
 
 const NavButton = styled.div`
@@ -73,34 +73,17 @@ const NavButton = styled.div`
     display: flex;
     margin-left: auto;
     gap: 15px;
-    button{
-      background-color: transparent;
-      background-size: 25px;
-      background-position: center;
-      background-repeat: no-repeat;
-      border: 0;
-      width: 40px;
-      height: 40px;
-    }
   }
 `
 
-const NavButton2 = styled.div`
-  display: none;
-  @media screen and (max-width: 900px) {
-    display: block;
-    ${props => props.margin === 'left' ? 'margin-right: auto;' : 'margin-left: auto;'};
-    background: rgba(255, 104, 97, 0.1);
-    border-radius: 12px;
-    button{
-      background-color: transparent;
-      background-size: 20px;
-      background-position: center;  
-      background-repeat: no-repeat;
-      border: 0;
-      width: 40px;
-      height: 40px;
-    }
+const HeaderBtn = styled.button`
+  background-color: transparent;
+  border: 0;
+  width: 40px;
+  height: 40px;
+  
+  img {
+    height: fit-content;
   }
 `
 
@@ -143,175 +126,96 @@ const SearchBtn = styled.button`
   width: 20px;
   margin-left: auto;
   border: 0;
-  background-image: url("${SearchImg.src}");
+  background: url("${SearchImg.src}") no-repeat center;
   background-size: 15px;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-color: transparent;
   opacity: 0.5;
 `
 
-const MenuTab = styled.div`
-  display: none;
-  @media screen and (max-width: 900px) {
-    display: block;
-    width: 100%;
-    padding-top: 30px;
-    a {
-      text-decoration: none;
-      cursor: pointer;
-    }
-    ul {
-      display: flex;
-      height: 40px;
-      line-height: 40px;
-      box-sizing: border-box;
-      border-bottom: 1px solid rgba(0, 0, 0, 0.06);
-    }
-    li {
-      width: 33.3%;
-    }
-  }
-`
-
-const MenuTabLi = styled.li`
-  border-bottom: ${props => props.isSelected ? '2px solid #1890FF;' : '0'}
-`
-
-const BtnHeaderSearch = styled.button`
-  background-image: url("${SearchImg.src}");
-`
-
-const BtnHeaderMenu = styled.button`
-  background-image: url("${MenuImg.src}");
-`
-
-const BtnHeaderMenu2 = styled.button`
-  background-image: url("${FilterImg.src}");
-`
-
-const BtnHeaderAccount = styled.button`
-  background-image: url("${AccountImg.src}");
-`
-
 const Header = () => {
+  const router = useRouter()
   const [headerView, setHeaderView] = useState(3)
   const [isSearch, setIsSearch] = useState(false)
-
-  const [isAgenda, setIsAgenda] = useState(false)
-  const [isWdd, setIsWdd] = useState(false)
-  const [isReference, setIsreference] = useState(false)
   const [isMenu, setIsMenu] = useState(false)
+  const [isMain, setIsMain] = useState(false)
+
+  const pathName = router.pathname
+
+  useEffect(() => {
+    if(pathName === '/'){
+      setIsMain(true)
+    }else{
+      setIsMain(false)
+    }
+  }, [router.pathname])
 
   const onSearch = () => {
     setIsSearch(!isSearch)
-  }
-
-  const onSelectedTab = (tab) => {
-    setIsAgenda(false)
-    setIsWdd(false)
-    setIsreference(false)
-
-    if(tab === 'agenda'){
-      setIsAgenda(true)
-    } else if(tab === 'wdd'){
-      setIsWdd(true)
-    } else if(tab === 'reference'){
-      setIsreference(true)
-    }
   }
 
   const onChangeHeader = (state) => {
     setHeaderView(state)
     setIsMenu(false)
   }
+
   return (
     <Wrapper>
       <Nav headerView={headerView}>
-        {headerView === 3 ?
-          <NavButton2 margin={'left'}><BtnHeaderMenu2 onClick={() => setIsMenu(true)}></BtnHeaderMenu2></NavButton2>
-          : <></>
-        }
-        <Link href="/" onClick={() => onSelectedTab('main')}>
-          <Logo />
+        <Link href="/">
+          <Logo isMain={isMain}/>
         </Link>
-        {headerView === 3 ?
-          <NavButton2><Link href="/#"><BtnHeaderAccount onClick={onSearch} /></Link></NavButton2>
-          : <NavButton>
-            {headerView === 1 ?
-              <BtnHeaderSearch onClick={onSearch} />
-              : <></>
-            }
-            <BtnHeaderMenu onClick={() => setIsMenu(true)} />
-          </NavButton>
-        }
-        <NavBox>
+        <NavButton>
+          <HeaderBtn onClick={onSearch}>
+            <Image src={isMain ? MainSearchImg : SearchImg} alt={'search btn'} />
+          </HeaderBtn>
+          <HeaderBtn onClick={() => setIsMenu(true)}>
+            <Image src={isMain ? MainMenuImg : MenuImg} alt={'menu btn'} />
+          </HeaderBtn>
+        </NavButton>
+        <NavBox isMain={isMain}>
+          <Link href="#">
+            우동디 이야기
+          </Link>
           <Link href="/agenda">
-            질문 목록
+            토론하기
           </Link>
           <Link href="/reference">
-            정책 정보
+            디지털정책
+          </Link>
+          <Link href="#">
+            뉴스레터
           </Link>
           <Link href="/search">
-            검색
+            <HeaderBtn onClick={onSearch}>
+              <Image src={isMain ? MainSearchImg : SearchImg} alt={'search btn'} />
+            </HeaderBtn>
           </Link>
         </NavBox>
       </Nav>
-      <Tools>
+      {/* <Tools>
         <Account />
-      </Tools>
-      { headerView === 3 && isSearch ?
-        <SearchForm>
-          <SearchInput placeholder={'검색어를 입력해주세요.'}></SearchInput>
-          <SearchBtn />
-        </SearchForm>
-        : isSearch ?
-          <>
-            <SearchForm>
-              <SearchInput placeholder={'검색어를 입력해주세요.'}></SearchInput>
-              <SearchBtn />
-            </SearchForm>
-            <RecentKeywords>
-              <dl>
-                <dt>최근검색어</dt>
-                <dd>keywords</dd>
-                <dd>keywords1</dd>
-                <dd>keywords2</dd>
-              </dl>
-            </RecentKeywords>
-          </>
-          : <></>
+      </Tools> */}
+      { isSearch &&
+        <>
+          <SearchForm>
+            <SearchInput placeholder={'검색어를 입력해주세요.'}></SearchInput>
+            <SearchBtn />
+          </SearchForm>
+          <RecentKeywords>
+            <dl>
+              <dt>최근 검색어</dt>
+              <dd>keywords</dd>
+              <dd>keywords1</dd>
+              <dd>keywords2</dd>
+            </dl>
+          </RecentKeywords>
+        </>
       }
-      {headerView === 2 ?
-        <MenuTab>
-          <ul>
-            <MenuTabLi isSelected={isAgenda}>
-              <Link href="/agenda" onClick={() => onSelectedTab('agenda')}>
-                토론장
-              </Link>
-            </MenuTabLi>
-            <MenuTabLi isSelected={isWdd}>
-              <Link href="#" onClick={() => onSelectedTab('wdd')}>
-                우동디 이야기
-              </Link>
-            </MenuTabLi>
-            <MenuTabLi isSelected={isReference}>
-              <Link href="/reference" onClick={() => onSelectedTab('reference')}>
-                데이터 정책
-              </Link>
-            </MenuTabLi>
-          </ul>
-        </MenuTab>
-        : <></>
-      }
-      {isMenu ?
+      { isMenu &&
         <NavMenu
           setIsMenu={setIsMenu}
           onChangeHeader={onChangeHeader}
           headerView={headerView}/>
-        : <></>
       }
-
     </Wrapper>
   )
 }
