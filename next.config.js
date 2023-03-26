e513bdc1bf5d18acc9be1e34c620f72801569855
sl kim <sl@lxf.kr>
2023-03-27 08:25:44 +0900

    SVG 컴퍼넌트 지원 추가

--- next.config.js
+++ next.config.js
@@ -9,4 +9,22 @@ module.exports = {
     FB_CLIENT_ID: process.env.FB_CLIENT_ID,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   },
+  webpack: config => {
+    const imageRule = config.module.rules.find(rule => rule.test?.test('.svg'))
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      resourceQuery: /inline/,
+      issuer: imageRule.issuer,
+      use: ['@svgr/webpack'],
+    })
+
+    config.module.rules.push({
+      ...imageRule,
+      test: /\.svg$/,
+      resourceQuery: { not: /inline/ },
+    })
+
+    return config
+  },
 }
