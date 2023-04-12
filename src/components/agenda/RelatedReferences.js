376babdefad749b28394775da0fce23acd0ca05b
hupark <phwslk@gmail.com>
2023-04-12 10:49:36 +0900

    agenda 콘텐츠 상세 관련 정책 누락 추가 적용

--- src/components/agenda/RelatedReferences.js
+++ src/components/agenda/RelatedReferences.js
@@ -1,19 +1,40 @@
+import styled from '@emotion/styled'
+
+const Wrapper = styled.ul`
+  padding-left: 15px;
+  margin: 0;
+  margin-top: 15px;
+  display: grid;
+  gap: 5px;
+  
+  li{
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.01em;
+    list-style-type: disc;
+    color: #4D4D4D;
+  }
+  
+  a{
+    text-decoration: none;
+  }
+`
 
 const RelatedReferences = () => (
-  <ul>
+  <Wrapper>
     <li>
-      <a href="#_">[정책] 공인인증서가 공동인증서로 바뀌었습니다</a>
+      <a href="#_">공인인증서가 공동인증서로 바뀌었습니다</a>
     </li>
     <li>
-      <a href="#_">[사례] 2천만명의 처방 받은 약품 정보가 해외 업체에 판매 되었습니다</a>
+      <a href="#_">2천만명의 처방 받은 약품 정보가 해외 업체에 판매 되었습니다</a>
     </li>
     <li>
-      <a href="#_">[사례] 출입국 심사시에 찍었던 사진을 민간 기업에 제공하였습니다</a>
+      <a href="#_">출입국 심사시에 찍었던 사진을 민간 기업에 제공하였습니다</a>
     </li>
     <li>
-      <a href="#_">[사례] 영국 정부 gov.uk의 가이드라인</a>
+      <a href="#_">영국 정부 gov.uk의 가이드라인</a>
     </li>
-  </ul>
+  </Wrapper>
 )
 
 export default RelatedReferences
