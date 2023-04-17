f00a71d708bd4333e39a829c606094256e6ab5ea
hupark <phwslk@gmail.com>
2023-04-17 14:02:40 +0900

    theme tablet media 쿼리 기준 변경

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ import { createTheme } from '@mui/system'
 
 export const mq = {
   mobile: '@media (max-width: 600px)',
-  tablet: '@media (max-width: 900px)',
+  tablet: '@media (min-width: 601px) and (max-width: 900px)',
   desktop: '@media (min-width: 901px)',
 }
 
